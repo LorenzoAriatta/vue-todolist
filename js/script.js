4e2cd33b4445026b72c265083d091cde3c8b5989
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-03-28 16:16:08 +0200

    aggiungo inpunt e button per la funzionalità nell'aggiungere altre task

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ console.log('JS OK!');
 const app = new Vue({
     el: '#app',
     data: {
+
         toDoList: [{
             text: 'fare la spesa',
             done: false
@@ -23,6 +24,28 @@ const app = new Vue({
             text: 'giocare a Elden Ring',
             done: false
         },
-        ]
+        ],
+        doneList: [],
+        index: 0,
+        newTask: ''
+    },
+    methods: {
+        taskDone(item) {
+            item.done = true;
+            this.doneList.push(item);
+            console.log(this.doneList);
+        },
+        lineDone(item) {
+            if (item.done === true) {
+                return 'line';
+            }
+        },
+        addNewTask() {
+            const newTask = this.newTask.trim();
+            if (this.newTask < 0) {
+                this.toDoList.push(newTask);
+                this.newTask = '';
+            }
+        }
     }
 });
