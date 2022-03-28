d5afe61a4b6f22197d8b091cc186ec99e8415817
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-03-28 15:04:20 +0200

    implemento il v-for per stampare l'array con gli oggeti

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log('JS OK!');
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        toDoList: [{
+            text: 'fare la spesa',
+            done: false
+        },
+        {
+            text: 'sala ore 20',
+            done: false
+        },
+        {
+            text: 'chiamare banca',
+            done: false
+        },
+        {
+            text: 'pagare multa',
+            done: false
+        },
+        {
+            text: 'giocare a Elden Ring',
+            done: false
+        },
+        ]
+    }
+});
